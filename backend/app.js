5f325c2004eca76dcd39b69645aed99f7627719b
Bogdan Draghia <bogdan.draghia@nortic.es>
2021-05-17 21:39:48 +0000

    socket fix, frontend is done for lector, resolved bugs

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,42 @@ const app = express();
 const cors = require("cors");
 const server = require("http").Server(app);
 const socket = require("./socket");
+const jsonfile = require("jsonfile");
+const configFile = "./config.json";
+const net = require("net");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/tara", async (req, res) => {
-  res.send("TARA");
+app.get("/defaultLector", async (req, res) => {
+  jsonfile
+    .readFile(configFile)
+    .then((data) => {
+      return res.send(data);
+    })
+    .catch((error) => console.log(error));
 });
-const startLectorServer = socket.serverLector;
+
+app.get("/statelector", async (req, res) => {
+  res.send(await socket.LectorState());
+});
+
+app.post("/manualchangelector", async (req, res) => {
+  //socket.changeLectorState();
+  let booleanVal = await socket.manualChangeLectorState(req.body.boolean);
+  res.send(booleanVal);
+});
+
+app.get("/triggeroff", async (req, res) => {
+  await socket.writeDataLector("{message:'TriggerOff',code:'None'}");
+  res.send("ok");
+});
+
+app.post("/lectorwrite", async (req, res) => {
+  console.log(req.body);
+  await socket.writeDataLector(req.body);
+  res.send("Write data succes");
+});
 
 server.listen(4001, "0.0.0.0", () => {
   console.log(`Started server on 4001`);
--- backend/socket.js
+++ backend/socket.js
@@ -1,31 +1,81 @@
-var net = require("net");
+const { write } = require("fs");
+const net = require("net");
 
 const serverLector = net.createServer();
 
-const serverScale = net.createServer();
+let connectedClients = [];
+let stateLector = false;
 
-const handdleConnection = (con) => {
-  console.log("new client connected from" + con.remoteAddress);
-};
-const handdleDisconect = () => {
-  console.log(" client ");
-};
-
-serverLector.on("connection", handdleConnection);
-serverScale.on("connection", handdleConnection);
-
-serverLector.on("close", handdleDisconect);
-serverScale.on("close", handdleDisconect);
-
-serverLector.listen("1234", () => {
-  console.log("server socket lector listenting on 1234");
+serverLector.on("connection", (socket) => {
+  console.log("New conexion on serverLector");
+  socket.on("data", async (chunk) => {
+    connectedClients.push(socket);
+    let buffer = Buffer.from(chunk, "hex");
+    // let ascii = buffer.toString("utf8");
+    // let convert = JSON.stringify(chunk);
+    if ((buffer = [2, 116, 3])) {
+      //start lector
+      await manualChangeLectorState(true);
+    } else {
+      //stop lector
+      //ascii == "y"
+      stateLector = false;
+    }
+  });
+  socket.on("end", () => {
+    console.log("Closing connection with the client");
+  });
+  socket.on("error", (err) => {
+    console.log(`Error: ${err}`);
+  });
 });
 
-serverScale.listen("1235", () => {
-  console.log("server socket scale listenting on 1235");
+//serverLector.emit("data");
+
+const serverBascula = net.createServer();
+
+serverBascula.on("connection", (socket) => {
+  console.log("New conexion on serverBascula");
+  socket.on("data", (chunk) => {
+    console.log(chunk);
+  });
+  socket.on("end", () => {
+    console.log("Closing connection with the client");
+  });
+  socket.on("error", (err) => {
+    console.log(`Error: ${err}`);
+  });
+});
+
+const writeDataLector = async (data) => {
+  console.log(data);
+  connectedClients[connectedClients.length - 1].write(data.toString(), "ascii");
+  serverLector.emit(data.toString());
+};
+const LectorState = async () => {
+  return stateLector;
+};
+const manualChangeLectorState = async (boolean) => {
+  stateLector = boolean;
+  return stateLector;
+};
+const changeLectorState = async () => {
+  return (stateLector = !stateLector);
+};
+
+serverLector.listen(1112, () => {
+  console.log("lector server started on 1112");
+});
+serverBascula.listen(1235, () => {
+  console.log("lector server started on 1235");
 });
 
 module.exports = {
+  manualChangeLectorState,
+  writeDataLector,
+  LectorState,
+  changeLectorState,
   serverLector,
-  serverScale,
+  serverBascula,
+  stateLector,
 };
